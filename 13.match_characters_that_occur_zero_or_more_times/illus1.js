c1678c5ebf95373092715c5245a673e4ad6736b5
Benson <tumlivein@gmail.com>
2019-08-22 20:08:18 +0800

    進度 11 ~ 13

---
+++ 13.match_characters_that_occur_zero_or_more_times/illus1.js
@@ -0,0 +1,7 @@
+let soccerWord = "gooooooal!";
+let gPhrase = "gut feeling!";
+let oPhrase = "over the moon";
+let goRegex = /[go*]/;
+console.log(soccerWord.match(goRegex));
+console.log(gPhrase.match(goRegex));
+console.log(oPhrase.match(goRegex));
